619c3fcfda13381254bb796f6341d78fcca854d3
MLopez <programadormlopez@gmail.com>
2023-11-24 21:03:42 -0300

    finish project realState

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const imagenes = document.querySelectorAll('.propiedad__imagen');
+
+window.addEventListener('scroll', () =>{
+    const scroll = this.scrollY / -20;
+    imagenes.forEach( (imagen) => {
+        imagen.style.backgroundPositionY = `${scroll}px`;
+    })
+});
